19347ab42d3179ac7c48a583f57b1a4031425e5e
Mikhail A <mikhail@landofcash.net>
2025-02-28 23:01:45 +0000

    fixed logo URL
    Signed-off-by: Mikhail A <mikhail@landofcash.net>

--- src/components/header/LogoMain.vue
+++ src/components/header/LogoMain.vue
@@ -4,7 +4,7 @@ import LogoIcon from '@/components/header/LogoIcon.vue'
 
 <template>
   <div class="flex items-center space-x-2">
-    <a href="/public" class="flex items-center space-x-2">
+    <a href="/" class="flex items-center space-x-2">
     <LogoIcon class="w-6 h-6 text-blue-400" />
     <span class="text-lg font-bold text-white">Bubble Volume Market Charts</span>
     </a>
